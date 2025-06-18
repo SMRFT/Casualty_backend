5772ed368ee9d07162754df2392b40fca1e90a05
chandra-frontend-dev <chandrasmrft@gmail.com>
2025-06-18 17:46:54 +0530

    Update urls.py

--- Casualty_backend/urls.py
+++ Casualty_backend/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('casualty.urls')),
     path('_b_a_c_k_e_n_d/casualty/', include('casualty.urls')),
+    path('', include('casualty.urls')),
+
 ]
